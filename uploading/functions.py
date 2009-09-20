89999ab860858e9427be16a002e4a00710ceebc3
Bombpersons <blindrabbits@gmail.com>
2009-09-20 17:03:47 +0100

    Made the uploader rename files to md5 hashes so that files won't clash.

--- uploading/functions.py
+++ uploading/functions.py
@@ -1,23 +1,33 @@
 from ankiResource import settings
 
-import os
+import os, shutil, hashlib, re
 
 #Stores the file in the media dir. Returns the location (relative to media dir) if succesfull.
 def storeFile(file, dir):
-	print os.path.join(settings.MEDIA_ROOT, dir, file.name)
 	#open the destination file
-	dest = open(os.path.join(settings.MEDIA_ROOT, dir, file.name), "wb+")
+	temp = open(os.path.join(settings.MEDIA_ROOT, 'temp', file.name), "wb+")
 	
 	#if we couldn't open the file return a blank string
-	if not dest:
+	if not temp:
 		return ""
 	
 	#now write to the file
 	for chunk in file.chunks():
-		dest.write(chunk)
-		
+		temp.write(chunk)
+	
+	#get the checksum for the file
+	hash =  hashlib.md5(temp.read()).hexdigest()
+	
 	#close the file
-	dest.close()
+	temp.close()
+	
+	#get the file extension
+	ext = file.name.split(".")
+	proper_filename = hash + "." + ext[len(ext) - 1]
+	
+	#now mv temp to proper location
+	shutil.move(os.path.join(settings.MEDIA_ROOT, 'temp', file.name), 
+					os.path.join(settings.MEDIA_ROOT, dir, proper_filename))
 	
 	#return the location of the file
-	return os.path.join(dir, file.name)
+	return os.path.join(dir, proper_filename)
