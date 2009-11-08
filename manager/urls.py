b8c071829566263e4fdaa8dc3f13fc7cb0583757
xuanji <xuanji@gmail.com>
2009-11-08 18:17:27 +0800

    got start_list.js posting changes to server but getting 500s (will look into it later)

--- manager/urls.py
+++ manager/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
 	url(r'^$', 'ankiResource.manager.views.index', name="url_manager_index"),
 	
 	# ** AJAX VIEWS **
-	url(r'ajax/modify$', 'ankiResource.manager.views.modify', name="url_manager_ajax_modify"),
+	url(r'^ajax/modify/$', 'ankiResource.manager.views.modify', name="url_manager_ajax_modify"),
 
 	# Start with a list
 	url(r'^list/(?P<start_list_id>\d+)$', 'ankiResource.manager.views.start_list', name="url_manager_start_list"),
--- static/js/start_list.js
+++ static/js/start_list.js
@@ -1,5 +1,77 @@
 dojo.require("dojo.dnd.Source");
 
+function handle_dnd_drop(source, nodes, copy, target) {
+	if(dojo.dnd.manager().target !== this){
+		return;
+	}
+
+	console.log("dropped!");
+	
+	if (!copy) {
+		precalc_sentence_map(source);
+	}
+	
+	var old_sentence_map = target.sentence_map;
+	precalc_sentence_map(target);
+	var new_sentence_map = target.sentence_map;
+
+	var changed_sentences = [];
+	
+	for (i in new_sentence_map) {
+		if (!(i in old_sentence_map)) {
+			changed_sentences.push(i);
+		}
+	}
+	
+	var sentences_string = "";
+	for(var i = 0; i < changed_sentences.length; ++i){
+		sentences_string = sentences_string + " " + changed_sentences[i]; 
+	}
+	
+	if (copy) {
+		update_server_copy(this.list_id, sentences_string);
+	}
+	else {
+		update_server_move(source.list_id, this.list_id, sentences_string);
+	}
+	
+}
+
+function update_server_move(source_list_id, dest_list_id, changed_sentences) {
+	console.log(changed_sentences);
+	console.log(" moved from " + source_list_id + " to " + dest_list_id);
+	
+	var POSTcontent = [];
+	POSTcontent["action"] = "move";			
+	POSTcontent["source"] = source_list_id;
+	POSTcontent["dest"] = dest_list_id;
+	POSTcontent["sentences"] = changed_sentences;
+	
+	xhrArgs={
+		url: "/manager/ajax/modify",
+		handleAs: "text",
+		content: POSTcontent,
+		
+		load: function(data,args){
+        	console.log("success");
+		},
+		
+		// if any error occurs, it goes here:
+		error: function(error,args){
+			console.warn("error!",error);
+		},
+	}
+	
+	dojo.xhrPost(xhrArgs);
+	
+	
+}
+
+function update_server_copy(dest_list_id, changed_sentences) {
+	console.log(changed_sentences);
+	console.log(" copied to " + dest_list_id); 
+}
+
 function create_dnd_from_list(response_data, list_id) {
 	var list_name = "list_" + list_id;
 	var dnd_dom = create_dnd_dom(list_name, response_data[1]); 
@@ -7,21 +79,20 @@ function create_dnd_from_list(response_data, list_id) {
 	var dnd_source = new dojo.dnd.Source(dnd_dom, {accept: ["editable"], creator:custom_creator});
 	dnd_source.checkAcceptance = custom_checkAcceptance;
 	dnd_source.insertNodes(false, response_data[0]);
+	dnd_source.list_id = list_id;
 	precalc_sentence_map(dnd_source);
+	dojo.connect(dnd_source, "onDndDrop", handle_dnd_drop);
 }
 
 function precalc_sentence_map(source) {
 	//for each item in source.map, extracts item.data.sentence into source.sentence_map
-	var dict_array = new Array();
+	console.log("precalculating");
+	var dict_array = {};
     source.forInItems( function (item) {	//source.map accessed through this function
-        dict_array.push(item.data.sentence);
+        dict_array[item.data.sentence]=1;
     });
     
     source.sentence_map = dict_array;
-    
-    //console.log(4 in dict_array);
-	//console.dir(source);
-	
 }
 
 function get_list(list_id) {
@@ -52,30 +123,23 @@ function create_dnd_dom(name, title) { //returns a DOMnode of the ol
 var custom_creator = function(item, hint){
 	var node;
 	node = dojo.doc.createElement("li");
-	node.innerHTML = item.data;
-	node.id = dojo.dnd.getUniqueId();
+	node.innerHTML = item.data + item.sentence;	//debug to see sentence-id of each sentence
+	//node.id = dojo.dnd.getUniqueId();
 	return {node: node, data: item, type: item.type};	
 //The default creator seems to set data: item.data, throwing away the "sentence" field, hence a custom creator is used here.
 };
 
 custom_checkAcceptance = function(source, nodes) {
-// this.map: 					array of items in the target
-// this.sentence_map: 			array of items in the source
-// source.getItem(nodes[i].id):	ith item in the nodes selected	
-	
-	var flag = true;
+
 			
 	for(var i = 0; i < nodes.length; ++i){
 		var sentence_id = source.getItem(nodes[i].id).data.sentence;
+		//console.log(sentence_id);
+		//console.dir(this.sentence_map);
 		if(sentence_id in this.sentence_map) {
-			console.log("collision");
+			//console.log("collision");
+			return false;
 		}
 	}
 	return true;
 }	
-
-
-
-
-
-
