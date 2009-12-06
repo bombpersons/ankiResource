2d121ad77e5823e5cc9c52cc404ecfa1c4c429ef
xuanji <xuanji@gmail.com>
2009-12-06 17:15:05 +0800

    added sentence highlighting when mousing over sentence or buttons

--- static/js/ajax_edit_list.js
+++ static/js/ajax_edit_list.js
@@ -1,38 +1,13 @@
-	dojo.require("dojo.NodeList-fx");
-	dojo.require("dijit.form.Button");
-    dojo.require("dijit.form.DropDownButton");
-    dojo.require("dijit.Menu");
-    dojo.require("dijit.form.TextBox");
+dojo.require("dojo.NodeList-fx");
+dojo.require("dijit.form.Button");
+dojo.require("dijit.form.DropDownButton");
+dojo.require("dijit.Menu");
+dojo.require("dijit.form.TextBox");
 
-	function add_to_list(sentence_id, list_id) {
-			var POSTcontent = new Array();
-			
-			POSTcontent["add"] = 1;			
-			POSTcontent["list"] = list_id;
-			POSTcontent["sentence"] = sentence_id;
-			
-			xhrArgs={
-				url: "/lists/ajax/list/add/",
-				handleAs: "text",
-				content: POSTcontent,
-				
-				load: function(data,args){
-                	dojo.byId("response"+sentence_id).innerHTML = "Added.";
-				},
-
-				error: function(error,args){
-					console.warn("error!",error);
-				},
-			}
-			
-			dojo.xhrPost(xhrArgs);
-            dojo.byId("response"+sentence_id).innerHTML = "Adding..."
-	}
-	
-	function remove_from_list(sentence_id, list_id) {
+function add_to_list(sentence_id, list_id) {
 		var POSTcontent = new Array();
 		
-		POSTcontent["remove"] = 1;			
+		POSTcontent["add"] = 1;			
 		POSTcontent["list"] = list_id;
 		POSTcontent["sentence"] = sentence_id;
 		
@@ -42,7 +17,7 @@
 			content: POSTcontent,
 			
 			load: function(data,args){
-            	dojo.byId("response"+sentence_id).innerHTML = "Removed.";
+            	dojo.byId("response"+sentence_id).innerHTML = "Added.";
 			},
 
 			error: function(error,args){
@@ -51,6 +26,31 @@
 		}
 		
 		dojo.xhrPost(xhrArgs);
-        dojo.byId("response"+sentence_id).innerHTML = "Removing..."
+        dojo.byId("response"+sentence_id).innerHTML = "Adding..."
+}
+
+function remove_from_list(sentence_id, list_id) {
+	var POSTcontent = new Array();
+	
+	POSTcontent["remove"] = 1;			
+	POSTcontent["list"] = list_id;
+	POSTcontent["sentence"] = sentence_id;
+	
+	xhrArgs={
+		url: "/lists/ajax/list/add/",
+		handleAs: "text",
+		content: POSTcontent,
+		
+		load: function(data,args){
+        	dojo.byId("response"+sentence_id).innerHTML = "Removed.";
+		},
+
+		error: function(error,args){
+			console.warn("error!",error);
+		},
 	}
+	
+	dojo.xhrPost(xhrArgs);
+    dojo.byId("response"+sentence_id).innerHTML = "Removing..."
+}
 
---
+++ static/js/highlight_sentence.js
@@ -0,0 +1,7 @@
+function highlight(id) {
+	dojo.style("sentence_text_" + id, "backgroundColor", "cyan");
+}
+
+function no_highlight(id) {
+	dojo.style("sentence_text_" + id, "backgroundColor", "#EEEEEE");
+}
